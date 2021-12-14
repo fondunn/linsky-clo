eb219ffa3f60d0c0698470a575ac4280ceb8ab58
Denis Karlinsky <64307212+fondunn@users.noreply.github.com>
2021-12-14 16:35:53 +0200

    update shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 import { Routes, Route} from 'react-router-dom'
 
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 
 const App = () => (
   
   <div>
   <Routes>
     <Route path='/' element={<HomePage />} />
-    {/* <Route path='/' element={<HomePage />} /> */}
+    <Route path='/shop' element={<ShopPage />} />
   </Routes>
     
   </div>
